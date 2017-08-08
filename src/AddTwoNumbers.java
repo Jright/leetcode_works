d92ea715089ef94ca8db913bdf966ea4157798f8
minchao.zhu <readthinkcommunicate@gmail.com>
2017-08-09 05:00:15 +0800

    Problem 2 Add two numbers

---
+++ src/AddTwoNumbers.java
@@ -0,0 +1,52 @@
+/**
+ * Created by jright on 2017/7/31.
+ */
+public class AddTwoNumbers {
+
+    //    You are given two non-empty linked lists representing two non-negative integers.
+//    The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+//
+//    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+//            Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+//    Output: 7 -> 0 -> 8
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        //进位
+//        int carry = 0;
+//        ListNode cur = new ListNode(0);
+//        while(l1.next != null || l2.next != null || carry != 0){
+//            carry = l1.val + l2.val;
+//
+//        }
+//        return cur;
+        ListNode prev = new ListNode(0);
+        ListNode head = prev;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            ListNode cur = new ListNode(0);
+            int sum = ((l2 == null) ? 0 : l2.val) + ((l1 == null) ? 0 : l1.val) + carry;
+            cur.val = sum % 10;
+            carry = sum / 10;
+            prev.next = cur;
+            prev = cur;
+
+            l1 = (l1 == null) ? l1 : l1.next;
+            l2 = (l2 == null) ? l2 : l2.next;
+        }
+        return head.next;
+    }
+
+    public static void main(String[] args){
+
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+}
