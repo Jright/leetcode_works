8c9432c512420e66bf5fa32b6a2e501ff7678278
jeremy.zhu <readthinkcommunicate@gmail.com>
2020-06-07 11:20:15 +0100

    add no.518 coin change 2

---
+++ src/No501_550/No518_CoinChange2.java
@@ -0,0 +1,19 @@
+package No501_550;
+
+public class No518_CoinChange2 {
+
+    public int change(int amount, int[] coins) {
+
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+
+        for(int coin: coins){
+            for(int i = coin; i <= amount; i++){
+                dp[i] += dp[i - coin];
+            }
+        }
+
+        return dp[amount];
+    }
+
+}
