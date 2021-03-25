cfbd3d3ac191f1eac4f48db3ef5c99516bb80404
jeremy.zhu <readthinkcommunicate@gmail.com>
2021-03-25 23:31:09 +0000

    update no.160 with less space complexity

--- src/No151_200/No160_IntersectionOfTwoLinkedLists.java
+++ src/No151_200/No160_IntersectionOfTwoLinkedLists.java
@@ -7,28 +7,66 @@ import java.util.HashMap;
 public class No160_IntersectionOfTwoLinkedLists {
 
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        HashMap<Integer, String> hashMap = new HashMap<>();
-
-        while (headA != null) {
-
-            hashMap.put(headA.val, headA.toString());
-            headA = headA.next;
-
+        if(headA == null || headB == null){
+            return null;
         }
 
-        while(headB != null){
+        int lenA = 0;
+        int lenB = 0;
 
-            String s = hashMap.get(headB.val);
-            if(s != null && s.equals(headB.toString())){
-                return new ListNode(headB.val);
-            }else{
+        ListNode countNodeA = headA;
+        ListNode countNodeB = headB;
+
+        while(countNodeA != null){
+            lenA++;
+            countNodeA = countNodeA.next;
+        }
+
+        while(countNodeB != null){
+            lenB++;
+            countNodeB = countNodeB.next;
+        }
+
+        int diff = Math.abs(lenA - lenB);
+
+        if(lenA > lenB){
+            while(diff > 0){
+                headA = headA.next;
+                diff--;
+            }
+
+            while(lenB > 0){
+                if(headA == headB){
+                    return headA;
+                }
+                headA = headA.next;
                 headB = headB.next;
+                lenB--;
+            }
+        }else if(lenA < lenB){
+            while(diff > 0){
+                headB = headB.next;
+                diff--;
+            }
+
+            while(lenB > 0){
+                if(headA == headB){
+                    return headA;
+                }
+                headA = headA.next;
+                headB = headB.next;
+                lenB--;
+            }
+        }else{
+            while(lenB > 0){
+                if(headA == headB){
+                    return headA;
+                }
+                headA = headA.next;
+                headB = headB.next;
+                lenB--;
             }
         }
-
         return null;
     }
-
-
-
 }
