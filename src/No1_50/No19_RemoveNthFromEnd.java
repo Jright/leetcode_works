de14167591a7c7abc48d5ffdf5c07c0d7dde5307
jeremy.zhu <readthinkcommunicate@gmail.com>
2021-03-26 00:08:15 +0000

    update no.19 with my own implementation

--- src/No1_50/No19_RemoveNthFromEnd.java
+++ src/No1_50/No19_RemoveNthFromEnd.java
@@ -4,24 +4,26 @@ import Data_Structures.ListNode;
 
 public class No19_RemoveNthFromEnd {
 
-    public ListNode removeNthFromEnd(ListNode head, int n) {//Note: Terrible question, what is this parameter 'head'? why return it when remove the nth
-    //element counting from the end? It just does not make any sense.
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(head == null || n < 1){
+            return null;
+        }
 
         ListNode dummy = new ListNode(0);
         dummy.next = head;
-        ListNode first = dummy;
-        ListNode second = dummy;
-        // Advances first pointer so that the gap between first and second is n nodes apart
-        for (int i = 1; i <= n + 1; i++) {
-            first = first.next;
-        }
-        // Move first to the end, maintaining the gap
-        while (first != null) {
-            first = first.next;
-            second = second.next;
-        }
-        second.next = second.next.next;
-        return dummy.next;
+        ListNode front = dummy;
+        ListNode tail = dummy;
 
+        while(n + 1> 0){
+            front = front.next;
+            n--;
+        }
+
+        while(front != null){
+            front = front.next;
+            tail = tail.next;
+        }
+        tail.next = tail.next.next;
+        return dummy.next;
     }
 }
