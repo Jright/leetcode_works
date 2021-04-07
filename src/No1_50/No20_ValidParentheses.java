2c7031edeb9e75da1777a59405ca12db5708a2c3
jeremy.zhu <readthinkcommunicate@gmail.com>
2021-04-07 19:54:25 +0100

    update no.20 valid parentheses

--- src/No1_50/No20_ValidParentheses.java
+++ src/No1_50/No20_ValidParentheses.java
@@ -1,75 +1,32 @@
 package No1_50;
 
-import java.util.Stack;
+import java.util.ArrayDeque;
 
 public class No20_ValidParentheses {
-
     public boolean isValid(String s) {
-//        ArrayList<Character> charList = new ArrayList<>();
-//        char[] chars = s.toCharArray();
-//        int i = 0;
-//        int j = 0;
-//        int k = 0;
-//
-//        for(Character c : chars){
-//
-//            if(c.equals('[')){
-//                charList.add(c);
-//                i++;
-//            }else if(c.equals('(')){
-//                charList.add(c);
-//                j++;
-//            }else if(c.equals('{')){
-//                charList.add(c);
-//                k++;
-//            }else if(c.equals(']')){
-//                 if(!charList.contains('[')){
-//                     return false;
-//                 }else{
-//                    i--;
-//                    if(i < 0){
-//                        return false;
-//                    }
-//                 }
-//            }else if(c.equals(')')){
-//                if(!charList.contains('(')){
-//                    return false;
-//                }else{
-//                    j--;
-//                    if(j < 0){
-//                        j = 0;
-//                        return false;
-//                    }
-//                }
-//            }else if(c.equals('}')){
-//                if(!charList.contains('{')){
-//                    return false;
-//                }else{
-//                    k--;
-//                    if(k < 0){
-//                        k = 0;
-//                        return false;
-//                    }
-//                }
-//            }else{
-//                return false;
-//            }
-//        }
-//        return true;
+        if (s == null || s.length() == 0) {
+            return false;
+        }
 
-        Stack<Character> stack = new Stack<Character>();
-        for (char c : s.toCharArray()) {
-            if (c == '(')
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
                 stack.push(')');
-            else if (c == '{')
+            } else if (s.charAt(i) == '{') {
                 stack.push('}');
-            else if (c == '[')
+            } else if (s.charAt(i) == '[') {
                 stack.push(']');
-            else if (stack.isEmpty() || stack.pop() != c)
-                return false;
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                if (stack.pop() != s.charAt(i)) {
+                    return false;
+                }
+            }
         }
         return stack.isEmpty();
     }
-
-
 }
