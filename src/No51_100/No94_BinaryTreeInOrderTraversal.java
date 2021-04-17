6ab072d1564e752637ddac300810fc810dc0065b
jeremy.zhu <readthinkcommunicate@gmail.com>
2021-04-17 12:13:39 +0100

    update no.94 binary tree in order traversal to use stack

--- src/No51_100/No94_BinaryTreeInOrderTraversal.java
+++ src/No51_100/No94_BinaryTreeInOrderTraversal.java
@@ -2,31 +2,29 @@ package No51_100;
 
 import Data_Structures.TreeNode;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
 
 public class No94_BinaryTreeInOrderTraversal {
-
-    List<Integer> ans = new ArrayList<>();
-
     public List<Integer> inorderTraversal(TreeNode root) {
-        if(root == null){
-            return ans;
+        if (root == null) {
+            return new ArrayList<>();
         }
 
-        helper(ans, root);
-        return ans;
-    }
+        List<Integer> res = new ArrayList<>();
+        ArrayDeque<TreeNode> stack = new ArrayDeque<TreeNode>();
 
-    private void helper(List<Integer> ans, TreeNode root){
-        if(root != null){
-            if(root.left != null){
-                helper(ans, root.left);
+        TreeNode node = root;
+        while (node != null || !stack.isEmpty()) {
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
             }
-            ans.add(root.val);
-            if(root.right != null){
-                helper(ans, root.right);
-            }
+            node = stack.pop();
+            res.add(node.val);
+            node = node.right;
         }
+        return res;
     }
 }
