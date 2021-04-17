5618e707a8e6452201f1f822f8a37fd2939eb99d
jeremy.zhu <readthinkcommunicate@gmail.com>
2021-04-17 19:45:45 +0100

    add no.144 binary tree preorder traversal 100% speed 66% space

---
+++ src/No101_150/No144_Binary_Tree_Preorder_Traversal.java
@@ -0,0 +1,33 @@
+package No101_150;
+
+import Data_Structures.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class No144_Binary_Tree_Preorder_Traversal {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<Integer>();
+        }
+
+        ArrayDeque<TreeNode> stack = new ArrayDeque<>();
+        List<Integer> result = new ArrayList<>();
+        TreeNode node = root;
+        stack.push(node);
+        while (!stack.isEmpty()) {
+            TreeNode popNode = stack.pop();
+            result.add(popNode.val);
+            if(popNode.left != null){
+                stack.push(popNode.left);
+            }
+            if(popNode.right != null){
+                stack.push(popNode.right);
+            }
+        }
+        return result;
+    }
+}
