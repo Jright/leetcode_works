543dfa5eff56d7f852fa6c6c55b446ee9b889768
jeremy.zhu <readthinkcommunicate@gmail.com>
2021-05-06 21:42:17 +0100

    add no.225 implement stack using queue 100% speed

---
+++ src/No201_250/No225_ImplementStackUsingQueues.java
@@ -0,0 +1,39 @@
+package No201_250;
+
+import java.util.LinkedList;
+
+public class No225_ImplementStackUsingQueues {
+
+    LinkedList<Integer> queue;
+
+    /** Initialize your data structure here. */
+    public No225_ImplementStackUsingQueues() {
+        queue = new LinkedList<>();
+    }
+
+    /** Push element x onto stack. */
+    public void push(int x) {
+        int size = queue.size();
+        queue.add(x);
+        while(size > 1){
+            queue.add(queue.remove());
+            size--;
+        }
+
+    }
+
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        return queue.remove();
+    }
+
+    /** Get the top element. */
+    public int top() {
+        return queue.peek();
+    }
+
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
