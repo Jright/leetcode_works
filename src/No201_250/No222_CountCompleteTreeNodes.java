cf3c445de9a816f175413a0235499125924c1370
jeremy.zhu <readthinkcommunicate@gmail.com>
2021-07-18 11:11:47 +0100

    update no.222 count complete tree nodes 100% speed 63% space

--- src/No201_250/No222_CountCompleteTreeNodes.java
+++ src/No201_250/No222_CountCompleteTreeNodes.java
@@ -4,22 +4,58 @@ import Data_Structures.TreeNode;
 
 public class No222_CountCompleteTreeNodes {
 
-    int result = 0;
-
     public int countNodes(TreeNode root) {
-        helper(root);
+        int result = 0;
+        int height = countHeight(root);
+        if(height <= 1){
+            return height;
+        }
+
+        result += (int) Math.pow(2, (height - 1)) - 1;
+
+        int left = 0;
+        int right = (int) Math.pow(2, (height - 1)) - 1;
+        int pivot;
+        while(left <= right){
+            pivot = left + (right - left) / 2;
+            if(exists(root, pivot, height)){
+                left = pivot + 1;
+            }else{
+                right = pivot - 1;
+            }
+        }
+
+        result += left;
         return result;
     }
 
-    private void helper(TreeNode node){
-        if(node == null){
-            return;
+    private int countHeight(TreeNode node){
+        int result = 0;
+
+        while(node != null){
+            result++;
+            node = node.left;
         }
 
-        result++;
+        return result;
+    }
 
-        helper(node.left);
-        helper(node.right);
+    private boolean exists(TreeNode node, int index, int height){
+        int left = 0;
+        int right = (int) Math.pow(2, (height - 1)) - 1;
+        int pivot;
+
+        for(int i = 0; i < (height - 1); i++){
+            pivot = left + (right - left) / 2;
+            if(index <= pivot){
+                node = node.left;
+                right = pivot;
+            }else{
+                node = node.right;
+                left = pivot + 1;
+            }
+        }
+        return node != null;
     }
 
 }
