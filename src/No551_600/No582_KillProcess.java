51988ae1ea525f5e8decd7dbcb26f26e29c8ddf8
jeremy.zhu <readthinkcommunicate@gmail.com>
2021-08-12 21:43:18 +0100

    add no.582 kill process 15% speed 5% space

---
+++ src/No551_600/No582_KillProcess.java
@@ -0,0 +1,39 @@
+package No551_600;
+
+import java.util.*;
+
+public class No582_KillProcess {
+
+    public List<Integer> killProcess(List<Integer> pid, List<Integer> ppid, int kill) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int index = 0;
+        for(; index < pid.size(); index++){
+            int parent = ppid.get(index);
+
+            if(!map.containsKey(parent)){
+                map.put(parent, new ArrayList<>());
+            }
+
+            int child = pid.get(index);
+            map.get(parent).add(child);
+        }
+
+        List<Integer> ans = new ArrayList<>();
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(kill);
+
+        while(!queue.isEmpty()){
+            int item = queue.poll();
+            ans.add(item);
+            List<Integer> childList = map.get(item);
+            if(childList != null){
+                for(Integer num : childList){
+                    queue.offer(num);
+                }
+            }
+        }
+
+        return ans;
+    }
+}
