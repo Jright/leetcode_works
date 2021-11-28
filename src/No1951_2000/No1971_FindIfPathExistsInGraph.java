c1f477fbf07e17bc76f144a9e1db4b11fffac51e
jeremy.zhu <readthinkcommunicate@gmail.com>
2021-11-28 09:48:44 +0000

    update no.1971 find path exists in graph 95% speed 91% space

--- src/No1951_2000/No1971_FindIfPathExistsInGraph.java
+++ src/No1951_2000/No1971_FindIfPathExistsInGraph.java
@@ -10,55 +10,56 @@ public class No1971_FindIfPathExistsInGraph {
             return true;
         }
 
-        Set<Integer> visited = new HashSet<>();
+        UnionFind set = new UnionFind(n);
+        for(int[] edge : edges){
+            set.union(edge[0], edge[1]);
+        }
 
-        Map<Integer, List<Integer>> map = new HashMap<>();
+        return set.areConnected(start, end);
 
-        for(int index = 0; index < edges.length; index++){
-            int[] edge = edges[index];
-            if(!map.containsKey(edge[0])){
-                List<Integer> list = new ArrayList<>();
-                list.add(edge[1]);
-                map.put(edge[0], list);
-            }else{
-                List<Integer> list = map.get(edge[0]);
-                list.add(edge[1]);
-                map.put(edge[0], list);
-            }
+    }
 
-            if(!map.containsKey(edge[1])){
-                List<Integer> list = new ArrayList<>();
-                list.add(edge[0]);
-                map.put(edge[1], list);
-            }else{
-                List<Integer> list = map.get(edge[1]);
-                list.add(edge[0]);
-                map.put(edge[1], list);
+    class UnionFind{
+        private int[] parent;
+        private int[] rank;
+
+        public UnionFind(int size){
+            this.parent = new int[size];
+            this.rank = new int[size];
+            for(int index = 0; index < this.parent.length; index++){
+                this.parent[index] = index;
+                this.rank[index] = 1;
             }
         }
 
-        Deque<Integer> stack = new ArrayDeque<>();
+        public boolean areConnected(int u, int v){
+            return find(u) == find(v);
+        }
 
-        stack.push(start);
-        while(!stack.isEmpty()){
-            int curr = stack.pop();
-            if(curr == end){
-                return true;
-            }
-
-            visited.add(curr);
-            List<Integer> list = map.get(curr);
-            if(list == null){
-                continue;
-            }
-
-            for(int index = 0; index < list.size(); index++){
-                if(visited.contains(list.get(index))){
-                    continue;
+        public void union(int u, int v){
+            if(u != v){
+                int a = find(u);
+                int b = find(v);
+                if(a != b){
+                    if(rank[a] > rank[b]){
+                        parent[b] = a;
+                        rank[a] += rank[b];
+                    }else{
+                        parent[a] = b;
+                        rank[b] += rank[a];
+                    }
                 }
-                stack.push(list.get(index));
             }
         }
-        return false;
+
+        private int find(int u){
+            int x = u;
+            while(x != this.parent[x]){
+                x = this.parent[x];
+            }
+
+            this.parent[u] = x;
+            return x;
+        }
     }
 }
