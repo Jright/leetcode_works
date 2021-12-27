5d6682e417b1f0f9e7e4992e3e2b1475bba6744b
jeremy.zhu <readthinkcommunicate@gmail.com>
2021-12-27 00:26:07 +0000

    update no.476 number complement 100% speed 82% space

--- src/No451_500/No476_NumberComplement.java
+++ src/No451_500/No476_NumberComplement.java
@@ -1,26 +1,22 @@
 package No451_500;
 
 public class No476_NumberComplement {
-
     public int findComplement(int num) {
         if(num == 0){
             return 1;
         }
 
-        int result = 0;
+        int res = 0;
         int shift = 0;
+        int curr = 0;
+        int bit = 0;
         while(num != 0){
-            int bit = (num & 1);
-            if(bit == 1){
-                bit = 0;
-            }else{
-                bit = 1;
-            }
-            result += (bit << shift);
+            bit = num & 1;
+            curr = bit == 1 ? 0 : 1;
+            res += (curr << shift);
             num = num >> 1;
             shift++;
         }
-
-        return result;
+        return res;
     }
 }
