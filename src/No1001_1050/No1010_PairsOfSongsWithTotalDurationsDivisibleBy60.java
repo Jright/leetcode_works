d7a3e00b0dd02ace284a19a413e63354df26b99a
jeremy.zhu <readthinkcommunicate@gmail.com>
2022-01-02 11:58:49 +0000

    add no.1026 pairs of songs with total durations divisible by 60 56% speed 10% space

---
+++ src/No1001_1050/No1010_PairsOfSongsWithTotalDurationsDivisibleBy60.java
@@ -0,0 +1,17 @@
+package No1001_1050;
+
+public class No1010_PairsOfSongsWithTotalDurationsDivisibleBy60 {
+    public int numPairsDivisibleBy60(int[] time) {
+        int[] remainders = new int[60];
+        int count = 0;
+        for(int t : time){
+            if(t % 60 == 0){
+                count += remainders[0];
+            }else{
+                count += remainders[60 - t % 60];
+            }
+            remainders[t % 60]++;
+        }
+        return count;
+    }
+}
