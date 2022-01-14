627bf66f178c294a944472f0fde3c472b2881788
jeremy.zhu <readthinkcommunicate@gmail.com>
2022-01-14 00:05:23 +0000

    add no.452 minimum arrows to burst arrows 9% speed 5% space

---
+++ src/No451_500/No452_MinimumArrowsToBurstArrows.java
@@ -0,0 +1,37 @@
+package No451_500;
+
+import java.util.Arrays;
+
+public class No452_MinimumArrowsToBurstArrows {
+    public int findMinArrowShots(int[][] points) {
+        if(points.length == 0){
+            return 0;
+        }
+
+        Arrays.sort(points, (o1, o2) -> {
+            if(o1[1] == o2[1]){
+                return 0;
+            }
+            if(o1[1] < o2[1]){
+                return -1;
+            }
+            return 1;
+        });
+
+        int arrowCount = 1;
+
+        int xStart, xEnd, firstEnd = points[0][1];
+
+        for(int[] p : points){
+            xStart = p[0];
+            xEnd = p[1];
+
+            if(firstEnd < xStart){
+                arrowCount++;
+                firstEnd = xEnd;
+            }
+        }
+
+        return arrowCount;
+    }
+}
