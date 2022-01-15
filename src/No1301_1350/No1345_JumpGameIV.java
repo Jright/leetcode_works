3f1a0e259d14f0cd3b8dfe924e438505ebaf747f
jeremy.zhu <readthinkcommunicate@gmail.com>
2022-01-15 16:00:11 +0000

    update no.1345 jump game IV 99% speed 72% space

--- src/No1301_1350/No1345_JumpGameIV.java
+++ src/No1301_1350/No1345_JumpGameIV.java
@@ -4,52 +4,64 @@ import java.util.*;
 
 public class No1345_JumpGameIV {
     public int minJumps(int[] arr) {
-        if(arr.length == 1){
+        int n = arr.length;
+        if(n == 1){
             return 0;
         }
 
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-
-        for(int index = 0; index < arr.length; index++){
-            graph.computeIfAbsent(arr[index], v -> new LinkedList<>()).add(index);
+        if(n == 2 || arr[0] == arr[n - 1]){
+            return 1;
         }
 
-        List<Integer> curr = new LinkedList<>();
-        curr.add(0);
-        Set<Integer> visited = new HashSet<>();
-        int step = 0;
+        if(arr[0] == arr[n - 2]){
+            return 2;
+        }
 
-        while(!curr.isEmpty()){
-            List<Integer> next = new LinkedList<>();
+        Queue<Integer> queue = new ArrayDeque<>();
+        HashMap<Integer, List<Integer>> indexMap = new HashMap<>();
 
-            for(int node : curr){
-                if(node == arr.length - 1){
-                    return step;
+        for(int index = 0; index < n; index++){
+            if(!indexMap.containsKey(arr[index])){
+                indexMap.put(arr[index], new ArrayList<>());
+            }
+            indexMap.get(arr[index]).add(index);
+        }
+
+        queue.add(n - 1);
+        boolean[] seen = new boolean[n];
+
+        int level = 0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int index = 0; index < size; index++){
+                int state = queue.poll();
+                if(state == 0){
+                    return level;
                 }
 
-                for(int child : graph.get(arr[node])){
-                    if(!visited.contains(child)){
-                        visited.add(child);
-                        next.add(child);
+                if(state + 1 < n && !seen[state + 1]){
+                    seen[state + 1] = true;
+                    queue.add(state + 1);
+                }
+
+                if (state - 1 >= 0 && !seen[state - 1]) {
+                    seen[state - 1] = true;
+                    if (state - 1 == 0){
+                        return level + 1;
                     }
+                    queue.add(state - 1);
                 }
 
-                graph.get(arr[node]).clear();
-
-                if(node + 1 < arr.length && !visited.contains(node + 1)){
-                    visited.add(node + 1);
-                    next.add(node + 1);
-                }
-
-                if(node - 1 >= 0 && !visited.contains(node - 1)){
-                    visited.add(node - 1);
-                    next.add(node - 1);
+                List<Integer> same = indexMap.get(arr[state]);
+                for (int i : same) {
+                    if (i == state || seen[i]) continue;
+                    seen[i] = true;
+                    if (i == 0) return level + 1;
+                    queue.add(i);
                 }
             }
-            curr = next;
-            step++;
+            level++;
         }
-
         return -1;
     }
 }
