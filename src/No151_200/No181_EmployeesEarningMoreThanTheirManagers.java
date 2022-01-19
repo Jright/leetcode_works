4fed549650d4057aa3253274e6e9fc565efc91c4
jeremy.zhu <readthinkcommunicate@gmail.com>
2022-01-19 20:10:57 +0000

    add no.181 employees earning more than their managers 25% speed copied answer

---
+++ src/No151_200/No181_EmployeesEarningMoreThanTheirManagers.java
@@ -0,0 +1,5 @@
+package No151_200;
+
+public class No181_EmployeesEarningMoreThanTheirManagers {
+//    SELECT a.NAME AS 'Employee' FROM Employee AS a JOIN Employee AS b ON a.ManagerId = b.Id AND a.Salary > b.Salary;
+}
