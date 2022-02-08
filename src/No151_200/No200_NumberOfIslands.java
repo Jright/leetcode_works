55d2d7e3f52c28377f2d2ad18bab18eeaf28bd70
jeremy.zhu <readthinkcommunicate@gmail.com>
2022-02-08 22:39:57 +0000

    update no.200 number of islands 53% speed 10% space

--- src/No151_200/No200_NumberOfIslands.java
+++ src/No151_200/No200_NumberOfIslands.java
@@ -2,34 +2,47 @@ package No151_200;
 
 public class No200_NumberOfIslands {
 
+    boolean[][] visited;
+
     public int numIslands(char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        visited = new boolean[m][n];
+
         int count = 0;
-        for(int i = 0; i < grid.length; i++){
-            for(int j = 0; j < grid[i].length; j++){
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(visited[i][j]){
+                    continue;
+                }
+
+                if(grid[i][j] == '0'){
+                    continue;
+                }
+
                 if(grid[i][j] == '1'){
                     count++;
-                    dfs(grid, i, j);
+                    dfs(i, j, m, n, grid);
                 }
             }
         }
+
         return count;
     }
 
-    private void dfs(char[][] grid, int i, int j) {
-
-        if(i < 0 || j < 0
-                || i > grid.length - 1|| j > grid[i].length - 1
-                || grid[i][j] == '0'){
+    private void dfs(int i, int j, int m, int n, char[][] grid){
+        if(i < 0 || j < 0 || i >= m || j >= n || visited[i][j]){
             return;
         }
 
-        grid[i][j] = '0';
+        visited[i][j] = true;
 
-        dfs(grid, i + 1, j);
-        dfs(grid, i, j + 1);
-        dfs(grid, i - 1, j);
-        dfs(grid, i, j - 1);
-
-
+        if(grid[i][j] == '1'){
+            dfs(i + 1, j, m, n, grid);
+            dfs(i, j + 1, m, n, grid);
+            dfs(i - 1, j, m, n, grid);
+            dfs(i, j - 1, m, n, grid);
+        }
     }
 }
