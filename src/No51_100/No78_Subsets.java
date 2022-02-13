eebacd9696093eb61ff3fcf8ab42231272037052
jeremy.zhu <readthinkcommunicate@gmail.com>
2022-02-13 12:19:46 +0000

    add no.78 subsets 13% speed 8% space copied answer

---
+++ src/No51_100/No78_Subsets.java
@@ -0,0 +1,22 @@
+package No51_100;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class No78_Subsets {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> output = new ArrayList<>();
+        output.add(new ArrayList<Integer>());
+
+        for (int num : nums) {
+            List<List<Integer>> newSubsets = new ArrayList<>();
+            for (List<Integer> curr : output) {
+                newSubsets.add(new ArrayList<Integer>(curr){{add(num);}});
+            }
+            for (List<Integer> curr : newSubsets) {
+                output.add(curr);
+            }
+        }
+        return output;
+    }
+}
