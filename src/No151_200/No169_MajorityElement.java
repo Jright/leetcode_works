0e8d571d0483d9171a62771fdd56848fad373a74
jeremy.zhu <readthinkcommunicate@gmail.com>
2022-02-21 00:36:19 +0000

    update no.169 majority element 64% speed 5% space

--- src/No151_200/No169_MajorityElement.java
+++ src/No151_200/No169_MajorityElement.java
@@ -3,39 +3,14 @@ package No151_200;
 import java.util.*;
 
 public class No169_MajorityElement {
-
-    public static int majorityElement(int[] nums) {
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for(int i = 0; i < nums.length; i++){
-            if(map.get(nums[i]) == null){
-                map.put(nums[i], 1);
-            }else{
-                map.put(nums[i], map.get(nums[i]) + 1);
-            }
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+        if(n == 1){
+            return nums[0];
         }
 
-        Iterator<Map.Entry<Integer, Integer>> iterator = map.entrySet().iterator();
+        Arrays.sort(nums);
 
-        int max = Integer.MIN_VALUE;
-        int key = -1;
-        while(iterator.hasNext()){
-            Map.Entry<Integer, Integer> next = iterator.next();
-            if(next.getValue() > max){
-                max = next.getValue();
-                key = next.getKey();
-            }
-        }
-
-        return key;
-
+        return nums[n / 2];
     }
-
-    public static void main(String[] args) {
-        int[] nums = new int[]{3, 2, 3};
-        int i = majorityElement(nums);
-        System.out.println(i);
-    }
-
 }
